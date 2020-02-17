190a5a74dfea45c79c22acb218a028e5cc52d195
Mihir Kelkar <mihirk107@gmail.com>
2020-02-17 00:21:28 -0600

    Done with the graphs

--- Assignment_2.py
+++ Assignment_2.py
@@ -4,11 +4,10 @@ Date: 2/6/2020
 """
 
 #Importing the libraries
-import time
 import struct as st
 import numpy as np
 import matplotlib.pyplot as plt
-from itertools import chain
+import copy
 
 np.random.seed(100)
 
@@ -24,14 +23,9 @@ train_labels = read_idx('train-labels.idx1-ubyte')
 test_data = read_idx('t10k-images.idx3-ubyte')
 test_labels = read_idx('t10k-labels.idx1-ubyte')
 
-#Initializing the parameters
-n = 60000
-learning_rate = 0.5
-e = 0.15
-W = np.random.uniform(-1, 1, size = (10, 784))
-epoch = 0
-errors_epoch = [0] * n
+W_old = np.random.uniform(-1, 1, size = (10, 784))
 
+#Onehotencoding the labels
 d = {}
 temp_enc = np.zeros((10,1))
 for x in range(10):
@@ -39,7 +33,7 @@ for x in range(10):
     temp_enc[x] = 1
     d[x] = temp_enc
 
-#The step activation function    
+#Implementing the step activation function    
 def step_act(y):
     for x in range(len(y)):
         if(y[x] >= 0):
@@ -48,36 +42,71 @@ def step_act(y):
             y[x] = 0
     return y
 
-#Training the neural network (Updating the weights)              
-while(True):
-    for i in range(n):
-        x = train_data[i].reshape(784,1)
-        temp = np.dot(W, x)
-        if(temp.argmax() != train_labels[i]):
-            errors_epoch[epoch] += 1
-            
-    epoch += 1
-    
-    for i in range(n):
-        x = train_data[i].reshape(784,1)
-        test = step_act(np.dot(W, x))
-        test1 = d[train_labels[i]]
-        test2 = (d[train_labels[i]] - step_act(np.dot(W, x)))
-        test3 = learning_rate * (d[train_labels[i]] - step_act(np.dot(W, x))) * x.T
-        W = W + learning_rate * (d[train_labels[i]] - step_act(np.dot(W, x))) * x.T
+def training(n, learning_rate, e):
+    #Training the neural network (Updating the weights)
+    W = copy.deepcopy(W_old)
+    epoch = 0
+    errors_epoch = [0] * n          
+    while(True):
         
-    print(errors_epoch[epoch - 1]/n)
-    if(errors_epoch[epoch - 1]/n <= e):
-        break
+        for i in range(n):
+            x = train_data[i].reshape(784,1)
+            temp = np.dot(W, x)
+            if(temp.argmax() != train_labels[i]):
+                errors_epoch[epoch] += 1
+                
+        epoch += 1
+        
+        for i in range(n):
+            x = train_data[i].reshape(784,1)
+            W = W + learning_rate * (d[train_labels[i]] - step_act(np.dot(W, x))) * x.T
+            
+        #print(errors_epoch[epoch - 1]/n)
+        if(errors_epoch[epoch - 1]/n <= e or epoch > 70):
+            break
+    
+    print("Number of epochs:",epoch)
+    epoch_arr = range(epoch)
+    plt.title("Epoch Number vs Number of Misclassifications")
+    plt.plot(epoch_arr, errors_epoch[0:epoch], c = 'black')
+    plt.xlabel("Epoch Number")
+    plt.ylabel("Number of Misclassifications")
+    plt.show()
+    
+    return W
+    
 
-#Testing the neural network
-no_of_errors = 0
-for i in range(len(test_data)):
-    x = test_data[i].reshape(784,1)
-    temp = np.dot(W, x)
-    if(temp.argmax() != test_labels[i]):
-        no_of_errors += 1
-       
-    
-    
+def test(W):    
+    #Testing the neural network
+    no_of_errors = 0
+    for i in range(len(test_data)):
+        x = test_data[i].reshape(784,1)
+        temp = np.dot(W, x)
+        if(temp.argmax() != test_labels[i]):
+            no_of_errors += 1
+    print("Percentage of testing error:", no_of_errors/len(test_data) * 100)
+
+W = training(100, 0.5, 0)
+test(W)
+
+W = training(50, 1, 0)
+test(W)    
+
+W = training(1000, 1, 0)
+test(W) 
+
+#training(60000, 1, 0)
+
+W_old = np.random.uniform(-1, 1, size = (10, 784))
+W = training(1000, 1, 0.5)
+test(W) 
+
+W_old = np.random.uniform(-1, 1, size = (10, 784))
+W = training(1000, 1, 0.25)
+test(W)
+
+W_old = np.random.uniform(-1, 1, size = (10, 784))
+W = training(1000, 1, 0.14)
+test(W)
+
 
