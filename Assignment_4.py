8cefcdc3e00b28db7a7d77b1997225682f89e86c
Mihir Kelkar <mihirk107@gmail.com>
2020-03-26 19:32:19 -0500

    Activations functions defined

--- Assignment_4.py
+++ Assignment_4.py
@@ -19,3 +19,42 @@ plt.xlabel("X")
 plt.ylabel("D")
 plt.show()
 
+#Initializing parameters
+w_init = np.random.uniform(0, 1, size = (24, 1))
+w_init_bias = np.random.uniform(0, 1, size = (24, 1))
+w_final = np.random.uniform(0, 1, size = (24, 1))
+w_final_bias = np.random.uniform(0, 1, size = (24, 1))
+u = []                                                 #Induced local field from initial layer
+z = []                                                 #Output from initial layer 
+p = 0                                                  #Induced local field from final layer
+y = 0                                                  #Output from final layer
+learning_rate = 0.1
+
+""" --- Activation Functions --- """
+
+#Feedforward
+def ff_init_act(u): 
+    return np.tanh(u)
+
+def ff_final_act(y):
+    return y
+
+#Feedback
+def fb_init_act(u):
+    return (1 - np.tanh(u) ** 2)
+
+def fb_final_act(y):
+    return 1
+
+""" --- Feedforward Training --- """
+def feed_forward():
+    
+    for i in range(300):
+        for j in range(24):
+            
+            
+            
+""" --- Main Algorithm --- """
+
+while(1):
+    y = feed_forward()
