085ac9041c6ce7faaad8446cafc46b7a6042d3c6
Mihir Kelkar <mihirk107@gmail.com>
2020-04-19 20:38:35 -0500

    Minor improvements done

--- Assignment_6.py
+++ Assignment_6.py
@@ -10,11 +10,23 @@ import cvxopt
 
 np.random.seed(100)
 
-#Initializing the input patterns and the desired output
+#Defining the required variables
 x = np.random.uniform(0, 1, (100, 2))
 d = np.zeros(100)
 class_one = []
 class_minusone = []
+sv_x = []
+sv_d = []
+sv_alpha = []
+summation = 0
+g = 0
+hyperplane = []
+p_hyperplane = []
+n_hyperplane = []
+x_coord = np.linspace(0.0, 1.0, num=1000)
+y_coord = np.linspace(0.0, 1.0, num=1000)
+
+#Assigning the input patterns to their respective classes
 for i in range(len(x)):
     if(x[i][1] < 1/5 * np.sin(10 * x[i][0]) + 0.3 or (x[i][1] - 0.8)**2 + (x[i][0] - 0.5)**2 < 0.15**2):
         d[i] = 1
@@ -26,12 +38,13 @@ for i in range(len(x)):
 class_one = np.asarray(class_one)
 class_minusone = np.asarray(class_minusone)        
 
-#PLotting the 
-plt.title("")
+#PLotting the input patterns
+fig, ax = plt.subplots(figsize=(10,10))
+plt.title("Input patterns")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
-plt.scatter(class_one[:, 0], class_one[:, 1], c = 'orange', label = "C1")
-plt.scatter(class_minusone[:, 0], class_minusone[:, 1], c = 'green', label = "C-1")
+plt.scatter(class_one[:, 0], class_one[:, 1], c = 'orange', label = "C1", marker = "P")
+plt.scatter(class_minusone[:, 0], class_minusone[:, 1], c = 'green', label = "C-1", marker = 0)
 plt.legend()
 plt.show()
 
@@ -52,86 +65,61 @@ for i in range(100):
     for j in range(100):
         K[i][j] = polynomial_kernel(x[i], x[j], 5)
 
+#Calculating the alpha values
 temp = np.ones(100) * -1
-P = cvxopt.matrix(np.outer(d,d) * K)
-q = cvxopt.matrix(temp)
-G = cvxopt.matrix(np.diag(temp))
-h = cvxopt.matrix(np.zeros(100))
-A = cvxopt.matrix(d, (1,100))
-b = cvxopt.matrix(0.0)
 
-#result = cvxopt.solvers.qp(P, q, G, h, A, b)
 result = cvxopt.solvers.qp(cvxopt.matrix(np.outer(d,d) * K), 
                            cvxopt.matrix(temp), cvxopt.matrix(np.diag(temp)), 
                            cvxopt.matrix(np.zeros(100)), 
                            cvxopt.matrix(d, (1,100)), 
                            cvxopt.matrix(0.0))
 
-#alpha = np.ravel(result['x'])
 alpha = np.asarray(result['x']).flatten()
-sv_cp1_x = []
-sv_cp1_y = []
-sv_cn1_x = []
-sv_cn1_y = []
-sv_x = []
-sv_y = []
-indices = []
+
+#Creating new lists based on support vectors
 for i in range(100):
     if(alpha[i] > 1e-5):
-        """if(d[i] == 1):
-            sv_cp1_x.append(x[i])
-            sv_cp1_y.append(d[i])
-        else:
-            sv_cn1_x.append(x[i])
-            sv_cn1_y.append(d[i])"""
         sv_x.append(x[i])
-        sv_y.append(d[i])
-        indices.append(i)
+        sv_d.append(d[i])
+        sv_alpha.append(alpha[i])
 
-#sv_x = sv_cp1_x + sv_cn1_x
-#sv_y = sv_cp1_y + sv_cn1_y
-
-summation = 0
-g = 0
-hyperplane = []
-p_hyperplane = []
-n_hyperplane = []
-alpha_sv = []
-d_sv = []
-for i in indices:
-    alpha_sv.append(alpha[i])
-    d_sv.append(d[i])
+#Finding the bias        
 for i in range(len(sv_x)):
-    summation += alpha_sv[i] * d_sv[i] * polynomial_kernel(sv_x[i], sv_x[1], 5)
-bias = sv_y[1] - summation
-print(bias)
+    summation += sv_alpha[i] * sv_d[i] * polynomial_kernel(sv_x[i], sv_x[0], 5)
+bias = sv_d[0] - summation
 
-
-x_coord = np.linspace(0.0, 1.0, num=1000)
-y_coord = np.linspace(0.0, 1.0, num=1000)
-
+#Creating the hyperplanes
 for i in range(len(x_coord)):
     print("Iteration:",i)
     for j in range(len(y_coord)):
         g = 0
         for k in range(len(sv_x)):
-            g += alpha_sv[k] * d_sv[k] * polynomial_kernel(sv_x[k], np.asarray([x_coord[i], y_coord[j]]), 5)
+            g += sv_alpha[k] * sv_d[k] * polynomial_kernel(sv_x[k], np.asarray([x_coord[i], y_coord[j]]), 5)
         g = g + bias
         
-        if -0.1 < g < 0.1:
+        if (-0.1 < g < 0.1):
             hyperplane.append([x_coord[i], y_coord[j]])
-        elif 0.9 < g < 1.1:
+        elif (0.9 < g < 1.1):
             p_hyperplane.append([x_coord[i], y_coord[j]])
-        elif -1.1 < g < -0.9:
+        elif (-1.1 < g < -0.9):
             n_hyperplane.append([x_coord[i], y_coord[j]])
     
 
+hyperplane = np.asarray(hyperplane)
+p_hyperplane = np.asarray(p_hyperplane)
+n_hyperplane = np.asarray(n_hyperplane)
+sv_x = np.asarray(sv_x)
+
+#Plotting the input patterns with hyperplanes
 fig, ax = plt.subplots(figsize=(10,10))
-plt.scatter(*zip(*class_one), c = 'red', label = 'Class 1')
-plt.scatter(*zip(*class_minusone), c = 'green', label = 'Class -1')
-plt.scatter(*zip(*p_hyperplane), c = 'red',s=1, label = 'Hyperplane 1')
-plt.scatter(*zip(*hyperplane), c = 'blue',s=1, label = 'Margin')
-plt.scatter(*zip(*n_hyperplane), c = 'green', s=1, label = 'Hyperplane -1')
-plt.scatter(*zip(*sv_x), facecolors = 'none', edgecolors='black',label='Support Vectors')
-plt.legend(loc = 'best')
-plt.show()        
+plt.title("Input patterns with hyperplanes")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.scatter(class_one[:, 0], class_one[:, 1], c = 'orange', label = "C1", marker = "P")
+plt.scatter(class_minusone[:, 0], class_minusone[:, 1], c = 'green', label = "C-1", marker = 0)
+plt.scatter(p_hyperplane[:, 0], p_hyperplane[:, 1], c = 'orange',s=1, label = 'Hyperplane 1')
+plt.scatter(hyperplane[:, 0], hyperplane[:, 1], c = 'red',s=1, label = 'Margin')
+plt.scatter(n_hyperplane[:, 0], n_hyperplane[:, 1], c = 'green', s=1, label = 'Hyperplane -1')
+plt.scatter(sv_x[:, 0], sv_x[:, 1], c = 'black', alpha = 0.8, label='Support Vectors')
+plt.legend()
+plt.show()   
